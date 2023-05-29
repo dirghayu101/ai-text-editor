dc2b01cbaf680ea6ada824aeb34bd324adf18da5
Dirghayu Joshi <dirghayujoshi47@gmail.com>
2023-05-29 18:56:46 +0530

    Model plan.

--- docs/Planning/Step-1.js
+++ docs/Planning/Step-1.js
@@ -4,12 +4,13 @@ So here are the steps:
 
 1. First of all I will setup all the things related to user authentication and authorization. I will do this in the backend.
     1.a> It will include these functionalities: User Login (validation) and User registration.
-    1.b> Email verification.
+    1.b> Email verification. Should answer yes to the question: Is the email real, does this person own the email?
     1.c> Forget Password and reset Password functionality.
     1.d> Two factors authentication
 
-2. I will create a corresponding User Interface for the above APIs that I will develop.
+2. I will create a corresponding User Interface for the above APIs.
 
+3. Then I will create an interface for the User Dashboard.
 
 
 */
