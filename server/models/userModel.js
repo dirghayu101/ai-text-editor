f3ca6b3f38d7739bea122af10be9215bdb80329d
Dirghayu Joshi <dirghayujoshi47@gmail.com>
2023-06-08 17:48:04 +0530

    Model definition.

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -2,6 +2,7 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
+  // userID will be generated (default)
   username: {
     type: String,
     required: [true, "Please enter a username"],
@@ -21,6 +22,36 @@ const userSchema = new mongoose.Schema({
   createdAt: {
     type: Date,
     default: Date.now,
+  },
+  // If set true, user will be able to access locked notes (his own).
+  accessLockedNotes: {
+    type: Boolean,
+    default: false,
+  },
+  // Password to access locked notes.
+  accessPin: {
+    type: String,
+    select: false,
+  },
+  // The note object.
+  // Category name as key and its content as value.
+  entryObject: {
+    type: Mixed,
+    select: false,
+  },
+  // If set true, the user should not be allowed to access their account.
+  blockAccount: {
+    type: Boolean,
+    default: true, //turn true, when the account gets verified.
+  },
+  // If this is false, block account should be true.
+  emailVerify: {
+    type: Boolean,
+    default: false,
+  },
+  //
+  deleteTime: {
+    type: Date,
   },
 });
 
